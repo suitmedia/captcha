ee067c848dc0f3cdbe8fbce06d9b7f6d0a4046ff
Wico Chandra <wico@suitmedia.com>
2016-02-18 13:27:11 +0700

    add EncryptCookies

--- src/routes.php
+++ src/routes.php
@@ -1,4 +1,10 @@
 <?php
-$this->app->router->get('captcha/image', ['as' => 'captcha', 'middleware' => '\Illuminate\Session\Middleware\StartSession', function () {
-    return \Captcha::CreateImage();
+$this->app->router->get('captcha/image', [
+    'as' => 'captcha',
+    'middleware' => [
+        '\Illuminate\Cookie\Middleware\EncryptCookies',
+        '\Illuminate\Session\Middleware\StartSession',
+    ],
+    function () {
+        return \Captcha::CreateImage();
 }]);
